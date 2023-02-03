eba6a8b562f7d6ef5df08e46ce69ba8bf6ad2455
Sterling <sterlingbarton81@gmail.com>
2023-02-02 20:59:15 -0600

    tied footer to bottom of page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content was brought to you by JavaScript";
+// const h2 = document.createElement('h2');
+// h2.textContent = "This content was brought to you by JavaScript";
 
-document.querySelector('body').appendChild(h2);
+// document.querySelector('body').appendChild(h2);
 

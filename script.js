44239b32a54ac8dd28d80ba40ca798e09cf6ad48
Sterling <sterlingbarton81@gmail.com>
2023-02-09 16:11:05 -0600

    added button movement

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-// const h2 = document.createElement('h2');
-// h2.textContent = "This content was brought to you by JavaScript";
 
-// document.querySelector('body').appendChild(h2);
+const contactBtn = document.querySelector('.btn');
+const floatingBtn = document.querySelector('.floating-button');
 
+$(window).scroll(function()
+{
+    console.log($(window).scrollTop())
+    if($(window).scrollTop() >= 300){
+        contactBtn.style.display = 'none';
+        floatingBtn.style.display = 'block';
+    } else{
+        contactBtn.style.display = 'block';
+        floatingBtn.style.display = 'none';
+    }
+    document.getElementById("fixed_box").innerHTML = "You have Scrolled to " + $(window).scrollTop() + "px";
+});
